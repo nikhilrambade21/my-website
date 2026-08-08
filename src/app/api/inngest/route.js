a6b83ba8ce1ac22bf3eb031aaa36efcf60a19ae9
Nikhil Rambade <nikhilrambade21@gmail.com>
2026-08-08 22:05:47 +0530

    fix

--- src/app/api/inngest/route.js
+++ src/app/api/inngest/route.js
@@ -1,13 +1,17 @@
 import { serve } from "inngest/next";
-import { inngest } from "@/config/inngest";
-import { syncUserCreation, syncUserDeletion, syncUserUpdation } from "../../../../config/inngest";
+
+import {
+  inngest,
+  syncUserCreation,
+  syncUserDeletion,
+  syncUserUpdation,
+} from "../../../../config/inngest";
 
 export const { GET, POST, PUT } = serve({
   client: inngest,
   functions: [
-
     syncUserCreation,
     syncUserUpdation,
-    syncUserDeletion
+    syncUserDeletion,
   ],
 });
