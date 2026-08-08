5d6f72afb8cc2092fc3cbdf8d5e09aaa37bf6866
Nikhil Rambade <nikhilrambade21@gmail.com>
2026-08-08 21:51:52 +0530

    user_information_databse

---
+++ models/user.js
@@ -0,0 +1,15 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    // creating objects for userSchema
+    _id : {type : String , required:true},
+    name : {type : String , required:true},
+    email : {type : String , required:true, unique:true},
+    imageUrl : {type : String , required:true},
+    cartItems : {type : object , default:{}}
+},{minimize:false})
+
+// creating user model 
+const User=mongoose.models.user || mongoose.model('user',userSchema)
+
+export default User
---
+++ src/app/api/inngest/route.js
@@ -0,0 +1,13 @@
+import { serve } from "inngest/next";
+import { inngest } from "@/config/inngest";
+import { syncUserCreation, syncUserDeletion, syncUserUpdation } from "../../../../config/inngest";
+
+export const { GET, POST, PUT } = serve({
+  client: inngest,
+  functions: [
+
+    syncUserCreation,
+    syncUserUpdation,
+    syncUserDeletion
+  ],
+});
