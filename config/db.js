f563a95d0aa1c6cc795a5dd0050e5d580ceb8f54
Nikhil Rambade <nikhilrambade21@gmail.com>
2026-08-09 16:49:38 +0530

    db_fix_again

--- config/db.js
+++ config/db.js
@@ -1,17 +1,17 @@
 import mongoose from "mongoose";
 
-let cached = global.mongoose;
+let cached = global.mongoose
 
 if (!cached) {
     cached = global.mongoose = {
         conn: null,
-        promise: null,
+        promise: null
     };
 }
 
 async function connectDB() {
     if (cached.conn) {
-        return cached.conn;
+        return cached.conn
     }
 
     if (!cached.promise) {
@@ -20,7 +20,7 @@ async function connectDB() {
         };
 
         cached.promise = mongoose.connect(
-            '${process.env.MONGODB_URI}/dirghayushoils',
+            `${process.env.MONGODB_URI}/dirghayushoils`,
             opts
         ).then((mongoose) => {
             return mongoose;
