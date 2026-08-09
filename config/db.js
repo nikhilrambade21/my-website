10346b7be371b591bdfce09a98ccf89e52dff9f3
Nikhil Rambade <nikhilrambade21@gmail.com>
2026-08-09 13:08:40 +0530

    db fix

--- config/db.js
+++ config/db.js
@@ -3,36 +3,33 @@ import mongoose from "mongoose";
 let cached = global.mongoose;
 
 if (!cached) {
-  cached = global.mongoose = {
-    conn: null,
-    promise: null,
-  };
+    cached = global.mongoose = {
+        conn: null,
+        promise: null,
+    };
 }
 
 async function connectDB() {
-  if (cached.conn) {
+    if (cached.conn) {
+        return cached.conn;
+    }
+
+    if (!cached.promise) {
+        const opts = {
+            bufferCommands: false,
+        };
+
+        cached.promise = mongoose.connect(
+            process.env.MONGODB_URI,
+            opts
+        ).then((mongoose) => {
+            return mongoose;
+        });
+    }
+
+    cached.conn = await cached.promise;
+
     return cached.conn;
-  }
-
-  if (!cached.promise) {
-    const opts = {
-      bufferCommands: false,
-    };
-
-    cached.promise = mongoose
-      .connect(process.env.MONGODB_URI, opts)
-      .then((mongoose) => {
-        return mongoose;
-      })
-      .catch((error) => {
-        cached.promise = null;
-        throw error;
-      });
-  }
-
-  cached.conn = await cached.promise;
-
-  return cached.conn;
 }
 
 export default connectDB;
